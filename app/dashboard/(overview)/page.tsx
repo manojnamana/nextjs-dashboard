9738abc07f52c73b1fe92a3b3d2117fdee724362
manoj <namanamanoj70@gmail.com>
2024-05-01 16:39:43 +0530

    add search bar

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,12 @@ import { Card } from '@/app/ui/dashboard/cards';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import {fetchCardData} from '@/app/lib/data'
 import { Suspense } from 'react';
 import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton } from '@/app/ui/skeletons';
 import { lusitana } from '@/app/ui/fonts';
  
 export default async function Page() {
-  const {totalPaidInvoices,totalPendingInvoices,numberOfInvoices,numberOfCustomers} = await fetchCardData()
+
 
 
   return (
